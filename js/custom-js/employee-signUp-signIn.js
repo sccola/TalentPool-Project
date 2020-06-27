1fe6052cbe6aa144f0f7e235cbabbf5a49ea10d3
sccola <sccola2020@gmail.com>
2020-06-27 18:03:12 +0100

    feat: consume sign up api

--- js/custom-js/employee-signUp-signIn.js
+++ js/custom-js/employee-signUp-signIn.js
@@ -1,21 +1,62 @@
 //starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    window.addEventListener('load', function () {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation')
-  
-      // Loop over them and prevent submission
-      Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
+  'use strict'
+
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation')
+    
+
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
           form.classList.add('was-validated')
-        }, false)
+
+          event.preventDefault()
+
+          // var formInputs = JSON.stringify({
+          var formInputs = {
+              firstName: form.firstname.value,
+              lastName: form.lastname.value,
+              email: form.email.value,
+              password: form.password.value,
+              // "confirm_password": form.confirm_password.value,
+              phoneNo: form.phone_no.value
+          };
+
+          var data = JSON.stringify(formInputs)
+
+
+      //  const formInputs = new FormData(document.getElementById('employeeSignUpForm'));
+
+      // const formInputs = JSON.stringify(Object.fromEntries(new FormData(event.target)));
+
+      // console.log(FormData);  
+
+      console.log(data); 
+      // console.log(JSON.stringify(formInputs));  
+      
+      fetch("https://api.lancers.app/v1/auth/employee-signup", {
+      // fetch("https://developers.lancers.app/v1/auth/employee-signup", {
+
+          method: 'post',
+          headers: {
+            "Accept": "application/json, text/plain",
+            "contentType": "application/json"
+            // 'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          // mode: "no-cors",
+          body: data
       })
-    }, false)
-  }())
-  
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+      }, false)
+    })
+  }, false)
+}())
